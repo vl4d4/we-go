168f00a45b7d558ffed51bd7629338f254f91387
vlada <vladimir.marinkovic89@gmail.com>
2018-11-24 14:07:30 +0100

    initial commit

---
+++ Tests/CompanySpecs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Web.Models;
+using Xunit;
+
+namespace Tests
+{
+    public class CompanySpecs
+    {
+        [Fact]
+        public void Companies_are_equal()
+        {
+            var company1 = new Company(1, "NAXI");
+            var company2 = new Company(1, "NAXI");
+
+            Assert.Equal(company1, company2);
+        }
+    }
+}
---
+++ Web/Models/Company.cs
@@ -0,0 +1,14 @@
+﻿namespace Web.Models
+{
+    public class Company
+    {
+        public long CompanyId { get; private set; }
+        public string CompanyName { get; private set; }
+
+        public Company(long id, string name)
+        {
+            CompanyId = id;
+            CompanyName = name;
+        }
+    }
+}
